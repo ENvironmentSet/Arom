39f74f06744e565bdf98ddbb16b120cdca860361
ENvironmentSet <sbshw1@gmail.com>
2020-08-08 23:15:06 +0900

    fix setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve('src', 'app.js'),
+  devtool: 'inline-cheap-source-map',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'cli.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  target: 'async-node'
+};
